9751d15deca4e1cb13f9ccb05666ad45dc504cfe
Wender Dalber <wender_dalber@hotmail.com>
2019-05-05 16:58:21 -0300

    create a method test

--- Unit_Test/UnitTest1.cs
+++ Unit_Test/UnitTest1.cs
@@ -1,4 +1,8 @@
+using app_booksearch.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 
 namespace Unit_Test
 {
@@ -8,6 +12,21 @@ namespace Unit_Test
         [TestMethod]
         public void TestMethod1()
         {
+        }
+
+        [TestMethod]
+        public bool OrderBy(string order)
+        {
+            try
+            {
+                var JSON = System.IO.File.ReadAllText(@"C:\Users\wende\Desktop\bitcoingroup\app_booksearch\app_booksearch\book.json");
+                var list = JsonConvert.DeserializeObject<List<Book>>(JSON);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
         }
     }
 }
