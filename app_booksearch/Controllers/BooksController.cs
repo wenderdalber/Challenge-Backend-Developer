7df805a831f6bada34350ebb5a9a1e8ac89af3a7
Wender Dalber <wender_dalber@hotmail.com>
2019-05-05 16:50:24 -0300

    start the project, with the list of books and search order by desc and asc

---
+++ app_booksearch/Controllers/BooksController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using app_booksearch.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace app_booksearch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<List<Book>> Get()
+        {
+            var JSON = System.IO.File.ReadAllText(@"C:\Users\wende\Desktop\bitcoingroup\app_booksearch\app_booksearch\book.json");
+            var list = JsonConvert.DeserializeObject<List<Book>>(JSON);
+            return list;
+        }
+
+        // GET api/values/asc
+        [HttpGet("{order}")]
+        public ActionResult<List<Book>> Get(string order)
+        {
+            var JSON = System.IO.File.ReadAllText(@"C:\Users\wende\Desktop\bitcoingroup\app_booksearch\app_booksearch\book.json");
+            var list = JsonConvert.DeserializeObject<List<Book>>(JSON);
+            if (order == "asc") { list = list.OrderBy(x => x.price).ToList(); }
+            else if (order == "desc") { list = list.OrderByDescending(x => x.price).ToList(); }
+            else
+            {
+                list = list.Where(x => x.name.Contains(order)).ToList();
+                if (list.Count == 0)
+                {
+                    list = list.Where(x => x.specifications.Author.Contains(order)).ToList();
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ app_booksearch/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app_booksearch.Models
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public decimal price { get; set; }
+        public specifications specifications { get; set; }
+    }
+
+    public class specifications
+    {
+        public string OriginallyPublished { get; set; }
+        public string Author { get; set; }
+        public int PageCount { get; set; }
+        public string[] Illustrator { get; set; }
+        public string[] Genres { get; set; }
+    }
+}
